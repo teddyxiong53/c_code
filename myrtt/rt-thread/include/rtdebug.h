053d7dd51d30d9f71c9ac106839c9fbd10faf726
teddyxiong53 <1073167306@qq.com>
2018-12-05 22:16:32 +0800

    把myrtt挪出去做单独的repo。

--- myrtt/rt-thread/include/rtdebug.h
+++
@@ -1,4 +0,0 @@
-#ifndef __RT_DEBUG_H__
-#define __RT_DEBUG_H__
-
-#endif
--- myrtt/rt-thread/src/thread.c
+++
@@ -1,9 +0,0 @@
-#include <rtthread.h>
-#include <rthw.h>
-#if 0
-rt_err_t rt_thread_init(
-	struct rt_thread* thread,
-	const char *name,
-	
-)
-#endif
