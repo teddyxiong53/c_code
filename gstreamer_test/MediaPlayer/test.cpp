9cadcdd0f2263b55056e1a3a00f88d74a4d03fee
teddyxiong53 <1073167306@qq.com>
2019-07-13 14:13:38 +0800

    不指定alsa设备，这样就是使用autoaudiosink了。不会打印之前的那个警告。使用USE_GLOOP，打开所有glib打印。

--- gstreamer_test/MediaPlayer/test.cpp
+++ gstreamer_test/MediaPlayer/test.cpp
@@ -1,15 +1,30 @@
 #include "GstMediaPlayer.h"
 #include <unistd.h>
 #include "GstUtils.h"
-
+#include <glib.h>
+#include <stdio.h>
+#include <string.h>
+#if 1
 int main()
 {
     GstUtils::initializeGStreamer();
     std::shared_ptr<GstMediaPlayer> mediaPlayer = nullptr;
-    mediaPlayer = GstMediaPlayer::create(GstMediaPlayer::StreamType::MP3_FILE, "test-gst", "default");
+    mediaPlayer = GstMediaPlayer::create(GstMediaPlayer::StreamType::MP3_FILE, "test-gst", "");
     mediaPlayer->prepare("file:///home/teddy/work/test/gstreamer/test/MediaPlayer/1.mp3");
     mediaPlayer->play();
     sleep(3);
     mediaPlayer->pause();
     return 0;
 }
+#else
+int main(int argc, char **argv)
+{
+    //g_log_set_handler(G_LOG_DOMAIN, G_LOG_LEVEL_WARNING| G_LOG_LEVEL_CRITICAL|G_LOG_LEVEL_DEBUG|G_LOG_LEVEL_INFO, g_log_default_handler, NULL);
+    g_warning("This is warning\n");
+    g_message("This is message\n");
+    g_debug("This is debug\n");
+    g_critical("This is critical\n");
+    g_log(NULL, G_LOG_LEVEL_INFO , "This is info\n");
+    return 0;
+}
+#endif
